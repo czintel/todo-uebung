15dd70f3364c1d3c66d14cf793617b6a2107e9a1
Christian Zintel <c.zintel@yahoo.de>
2021-07-03 17:29:25 +0200

    installed prettier. created components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import React, {useState} from 'react'
+import Form from './components/Form'
+import TodoList from './components/TodoList';
 
 function App() {
+  const [inputText, setInputText] = useState("")
   return (
     <div className="App">
-      <h1>Hello react</h1>
+      <header><h1>Hello react</h1></header>
+      <Form setInputText={setInputText} />
+      <TodoList inputText={inputText} />
     </div>
   );
 }
