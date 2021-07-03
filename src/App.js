fb1b298e4537681703692b4cb11efb77e1aa9389
Christian Zintel <c.zintel@yahoo.de>
2021-07-03 17:53:32 +0200

    worked on todo functionality

--- src/App.js
+++ src/App.js
@@ -3,15 +3,15 @@ import React, {useState} from 'react'
 import Form from './components/Form'
 import TodoList from './components/TodoList';
 
-function App() {
+export default function App() {
   const [inputText, setInputText] = useState("")
+  const [todos, setTodos] = useState([])
+
   return (
     <div className="App">
       <header><h1>Hello react</h1></header>
-      <Form setInputText={setInputText} />
-      <TodoList inputText={inputText} />
+      <Form todos={todos} setTodos={setTodos} inputText={inputText} setInputText={setInputText} />
+      <TodoList />
     </div>
   );
 }
-
-export default App;
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,15 +1,23 @@
 import React from 'react'
 
-const Form = () => {
+const Form = ({inputText, setInputText, todos, setTodos}) => {
 
-    const InputTextHandler = ({setInputText}) => {
-        setInputText()
+    const InputTextHandler = (e) => {
+        setInputText(e.target.value)
+    }
+
+    const submitTodoHandler = (e) => {
+        e.preventDefault()
+        setTodos([
+            ...todos, {text: inputText, completed: false, id: Math.random() * 1000 }
+        ])
+        setInputText('')
     }
 
     return (
         <form>
-        <input onChange={InputTextHandler} type="text" className="todo-input" />
-        <button className="todo-button" type="submit">
+        <input value={inputText} onChange={InputTextHandler} type="text" className="todo-input" />
+        <button onClick={submitTodoHandler}className="todo-button" type="submit">
           <i className="fas fa-plus-square"></i>
         </button>
         <div className="select">
